13368d4f6bbbfdeeebe5e8da9e93095baf917ead
lewisnjue <lewiskinyuanjue.ke@gmail.com>
2024-07-25 16:48:01 +0300

    aggregation

--- my_app/models.py
+++ my_app/models.py
@@ -1,20 +1,26 @@
-from django.db import models # any of my model calss will need to 
-""" 
-subclass the class django.db.models.models -> donf forget this 
-
- """
-
-# Create your models here.
-
-class BaseModel(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    class Meta:
-       abstract = True
+from django.db import models
 
 
-class Post(BaseModel):
-    title = models.CharField(max_length=30,verbose_name='Title Of Post')
-    comment = models.TextField()
-    class Meta:
-        ordering =['title']
-        verbose_name_plural ='posts'
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    def __str__(self):
+        return self.name
+
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=300)
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=300)
+    pages = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    rating = models.FloatField()
+    authors = models.ManyToManyField(Author)
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+    pubdate = models.DateField()
+
+
+class Store(models.Model):
+    name = models.CharField(max_length=30)
